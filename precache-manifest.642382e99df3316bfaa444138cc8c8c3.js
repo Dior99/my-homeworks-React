9de0058e93caf1cbf61d02e4d3ddab361113b609
Dior99 <n.shaldaev@yandex.ru>
2021-05-25 20:37:26 +0300

    Updates

---
+++ precache-manifest.642382e99df3316bfaa444138cc8c8c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb0c992a7a21b45d481ee3989074613",
+    "url": "/my-homeworks-React/index.html"
+  },
+  {
+    "revision": "4265d3934634b6427092",
+    "url": "/my-homeworks-React/static/css/main.98f59aa9.chunk.css"
+  },
+  {
+    "revision": "e2b951d906be4b4e2f15",
+    "url": "/my-homeworks-React/static/js/2.4dfc3d7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-homeworks-React/static/js/2.4dfc3d7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4265d3934634b6427092",
+    "url": "/my-homeworks-React/static/js/main.9713d77c.chunk.js"
+  },
+  {
+    "revision": "799680ede5a2cda57f7c",
+    "url": "/my-homeworks-React/static/js/runtime-main.a9a7fe03.js"
+  }
+]);
